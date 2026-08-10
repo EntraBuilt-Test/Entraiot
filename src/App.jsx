7de19a345f36a803e514427e06e3ffe363777e9a
Balamurugan200508 <balamurugan200508@gmail.com>
2026-08-10 15:53:54 +0530

    Add 5-second poster splash screen on application load

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import ITServices from './views/ITServices';
@@ -6,6 +6,23 @@ import IoTServices from './views/IoTServices';
 import Industries from './views/Industries';
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return (
+      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: '#fff', zIndex: 99999 }}>
+        <img src="/poster.jpeg" alt="Splash Poster" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <Routes>
